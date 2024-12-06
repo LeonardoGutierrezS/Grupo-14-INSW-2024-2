7757ab93ceb73fd1993ec08c47374dd36d7183b0
LuisFernandez <luis.fernandez2101@alumnos.ubiobio.cl>
2024-12-06 13:35:56 -0300

    ADD vendedores y middleware para vendedores y mecanicos

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {
   getUserService,
   getUsersService,
   registerEmployeeService,
+  registerSellerService,
   updateEmployeeStatusService,
   updateUserService,
   
@@ -137,6 +138,26 @@ export async function registerEmployee(req, res) {
     }
 
     const [newEmployee, errorEmployee] = await registerEmployeeService(body);
+
+    if (errorEmployee) {
+      return handleErrorClient(res, 400, "Error al registrar empleado", errorEmployee);
+    }
+
+    handleSuccess(res, 201, "Empleado registrado exitosamente", newEmployee);
+  } catch (error) {
+    handleErrorServer(res, 500, "Error del servidor al registrar empleado");
+  }
+}
+export async function registerSeller(req, res) {
+  try {
+    const { body } = req;
+    const { error } = userBodyValidation.validate(body);
+
+    if (error) {
+      return handleErrorClient(res, 400, "Error de validación", error.message);
+    }
+
+    const [newEmployee, errorEmployee] = await registerSellerService(body);
 
     if (errorEmployee) {
       return handleErrorClient(res, 400, "Error al registrar empleado", errorEmployee);
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -4,7 +4,7 @@ import { getEmployeeWorkHours,
   registerCheckOut, 
   updateCheckTime } from "../controllers/work_hours.controller.js";
 import { Router } from "express";
-import { isAdmin } from "../middlewares/authorization.middleware.js";
+import { isAdmin , isMechanic } from "../middlewares/authorization.middleware.js";
 import { checkActive } from "../middlewares/active.middleware.js";
 import { authenticateJwt } from "../middlewares/authentication.middleware.js";
 import {
@@ -13,6 +13,7 @@ import {
   getUser,
   getUsers,
   registerEmployee,
+  registerSeller,
   updateEmployeeStatus,
   updateUser,
   
@@ -29,7 +30,8 @@ router
   .get("/detail/", getUser)
   .patch("/detail/", updateUser)
   .delete("/detail/", deleteUser)
-  .post("/register-employee", isAdmin, registerEmployee)
+  .post("/register-mechanic", isAdmin, registerEmployee)
+  .post("/register-seller", isAdmin, registerSeller)
   .post("/check-in", checkActive,registerCheckIn)
   .post("/check-out", checkActive, registerCheckOut)
   .patch("/update-check-time/:id", isAdmin, updateCheckTime)
