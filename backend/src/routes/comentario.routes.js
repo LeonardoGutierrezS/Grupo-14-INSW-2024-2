d8b78648dbc29989dd3ccddf327cf7409f598439
maruru7 <maria.concha2001@alumnos.ubiobio.cl>
2024-12-23 22:30:45 -0300

    ADD auth tareas y comentarios

--- backend/src/routes/comentario.routes.js
+++ backend/src/routes/comentario.routes.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { actualizarComentario, crearComentario } from "../controllers/comentario.controller.js";
 import { eliminarComentario, obtenerComentarios } from "../controllers/comentario.controller.js";
+import { isAdmin , isMechanic } from "../middlewares/authorization.middleware.js";
+import { authenticateJwt } from "../middlewares/authentication.middleware.js";
 
 const router = express.Router();
+
+router.use(authenticateJwt);
 
 router.get("/", obtenerComentarios);
 router.post("/addcomentario", crearComentario);
 router.put("/:id_com", actualizarComentario);
-router.delete("/eliminar/:id_com", eliminarComentario);
+router.delete("/eliminar/:id_com", isAdmin, eliminarComentario);
 
 export default router;
--- backend/src/routes/tareas.routes.js
+++ backend/src/routes/tareas.routes.js
@@ -1,13 +1,17 @@
 "use strict"
 import express from "express";
 import { actualizarEstado, crearTarea } from "../controllers/tareas.controller.js";
-import { eliminarTarea, obtenerTodasTareas } from "../controllers/tareas.controller.js"
+import { eliminarTarea, obtenerTodasTareas } from "../controllers/tareas.controller.js";
+import { isAdmin , isMechanic } from "../middlewares/authorization.middleware.js";
+import { authenticateJwt } from "../middlewares/authentication.middleware.js";
 
 const router = express.Router();
 
-router.post("/crear", crearTarea);
+router.use(authenticateJwt)
+
+router.post("/crear", isAdmin, crearTarea);
 router.get("/", obtenerTodasTareas);
 router.patch("/estado/:id_tarea", actualizarEstado);
-router.delete("/eliminar/:id_tarea", eliminarTarea)
+router.delete("/eliminar/:id_tarea", isAdmin, eliminarTarea)
 
 export default router;
